365b750a57c339cf5b634e95cf364969dce4e290
Dheepa Sivakumaran <dheepasivakumaran@Dheepas-MacBook-Pro.local>
2018-10-30 19:40:25 -0400

    Add files

---
+++ keys.js
@@ -0,0 +1,19 @@
+//var spotify = new Spotify(keys.spotify);
+//var client = new Twitter(keys.twitter);
+
+var twitterKeys = {
+	consumer_key: "TBHb5NrWfFgUyywU1kEYeF88a",
+  	consumer_secret: "oZ0TPtaX9BvHFgCNIweLwkbil0def8edrU0RZkGFMxt9vD3rfc",
+  	access_token_key: "285726139-BkZWnsKIVfR6Cdzj8RnmoLcXOOCSDj3kYrdvtC64",
+  	access_token_secret: "8kTNs7o2ey94inZeUSy6hEWMwuYrjMYMg99EFaq7Va6lk"
+}
+
+var spotifyKeys = {
+	id: "c1c7d123adfe4c98981ba723ef8aea3c",
+	secret: "3e75b9f0c7d64937afcb9c69a047aa6a"
+}
+
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys
+}
